0c943410c277d4d5cc3947be0beb93e579574941
Zakariya khan <zak786khan@gmail.com>
2022-10-23 10:00:24 +0530

    Added Hero Section along with TailwindCss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import About from './components/About/About';
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <About />
     </>
   );
